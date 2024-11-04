3f3e565590ae7d7dfbd4f66cf0ac48de40e5702d
Jorin Vogel <hi@jorin.me>
2024-11-04 20:28:17 +0700

    fix error msg

--- server/handler/dashboard.go
+++ server/handler/dashboard.go
@@ -11,7 +11,7 @@ func ListDashboards(app *core.App) echo.HandlerFunc {
 	return func(c echo.Context) error {
 		result, err := core.ListDashboards(app, c.Request().Context())
 		if err != nil {
-			return c.JSONPretty(http.StatusBadRequest, struct{ Error error }{Error: err}, "  ")
+			return c.JSONPretty(http.StatusBadRequest, struct{ Error string }{Error: err.Error()}, "  ")
 		}
 		return c.JSONPretty(http.StatusOK, result, "  ")
 	}
@@ -21,7 +21,7 @@ func GetDashboard(app *core.App) echo.HandlerFunc {
 	return func(c echo.Context) error {
 		result, err := core.GetDashboard(app, c.Request().Context(), c.Param("name"))
 		if err != nil {
-			return c.JSONPretty(http.StatusBadRequest, struct{ Error error }{Error: err}, "  ")
+			return c.JSONPretty(http.StatusBadRequest, struct{ Error string }{Error: err.Error()}, "  ")
 		}
 		return c.JSONPretty(http.StatusOK, result, "  ")
 	}
