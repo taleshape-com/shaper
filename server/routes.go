682ac32ceb624c5ed6c5eeedd5b5014cae6ef1ea
Jorin Vogel <hi@jorin.me>
2024-11-10 18:43:19 +0700

    asset caching

--- server/routes.go
+++ server/routes.go
@@ -4,6 +4,7 @@ import (
 	"io/fs"
 	"shaper/core"
 	"shaper/server/handler"
+	"time"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -20,11 +21,29 @@ func routes(e *echo.Echo, app *core.App, frontendFS fs.FS) {
 		},
 	}))
 
+	// API routes - no caching
 	e.POST("/api/login/cookie", handler.CookieLogin(app))
 	apiWithAuth.GET("/login/cookie/test", handler.TestCookie)
 	apiWithAuth.GET("/dashboards", handler.ListDashboards(app))
 	apiWithAuth.GET("/dashboard/:name", handler.GetDashboard(app))
-	e.GET("/assets/*", frontend(frontendFS))
-	e.GET("/icon.svg", frontend(frontendFS))
-	e.GET("/*", indexHTML(frontendFS))
+
+	// Static assets - aggressive caching
+	assetsGroup := e.Group("/assets")
+	assetsGroup.Use(CacheControl(CacheConfig{
+		MaxAge:    365 * 24 * time.Hour, // 1 year
+		Public:    true,
+		Immutable: true,
+	}))
+	assetsGroup.GET("/*", frontendWithHeaders(frontendFS))
+
+	// Icon - moderate caching
+	iconGroup := e.Group("")
+	iconGroup.Use(CacheControl(CacheConfig{
+		MaxAge: 24 * time.Hour, // 1 day
+		Public: true,
+	}))
+	iconGroup.GET("/icon.svg", frontendWithHeaders(frontendFS))
+
+	// Index HTML - light caching with revalidation
+	e.GET("/*", indexHTMLWithCache(frontendFS))
 }
