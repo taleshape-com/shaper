f16afa5cde122f7f4bbf34d2944d8de6e2b2f485
Jorin Vogel <hi@jorin.me>
2024-11-11 13:09:02 +0700

    empty table

--- src/components/dashboard/DashboardTable.tsx
+++ src/components/dashboard/DashboardTable.tsx
@@ -20,9 +20,6 @@ type TableProps = {
 };
 
 function DashboardTable({ label, headers, data, sectionCount }: TableProps) {
-  if (!data) {
-    return <div>No data</div>;
-  }
   return (
     <div className={cx({
       "p-2 mb-3": true,
@@ -35,36 +32,49 @@ function DashboardTable({ label, headers, data, sectionCount }: TableProps) {
         </h2>
       }
       <Card className="p-2">
-        <TableRoot className={cx({
-          "overflow-auto": true,
-          ["max-h-[calc(100vh-9.4rem)]"]: label,
-          ["max-h-[calc(100vh-7.6rem)]"]: !label,
-        })}>
-          <Table>
-            <TableHead className="sticky top-0 bg-white shadow-sm">
-              <TableRow>
-                {headers.map((header) => (
-                  <TableHeaderCell key={header.name}>{header.name}</TableHeaderCell>
-                ))}
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {data.map((items, index) => (
-                <TableRow key={index} className={index % 2 === 0 ? "bg-slate-50" : undefined}>
-                  {items.map((item, index) => {
-                    const classes =
-                      typeof item === "number" ? "text-right" : "text-left";
-                    return (
-                      <TableCell key={index} className={classes}>
-                        {formatValue(item)}
-                      </TableCell>
-                    );
-                  })}
+        {!data ?
+          (
+            <div className={cx({
+              "h-20 py-1 px-3 flex items-center justify-center text-slate-600": true,
+              ["h-[calc(50vh-7.35rem)]"]: label,
+              ["h-[calc(50vh-5.6rem)]"]: !label,
+            })}>
+              no data
+            </div>
+          ) :
+          <TableRoot className={cx({
+            "overflow-auto": true,
+            ["max-h-[calc(100vh-9.4rem)]"]: label,
+            ["max-h-[calc(100vh-7.6rem)]"]: !label,
+          })}>
+            <Table>
+              <TableHead className="sticky top-0 bg-white shadow-sm">
+                <TableRow>
+                  {headers.map((header) => (
+                    <TableHeaderCell key={header.name}>{header.name}</TableHeaderCell>
+                  ))}
                 </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableRoot>
+              </TableHead>
+              <TableBody>
+                {
+                  data.map((items, index) => (
+                    <TableRow key={index} className={index % 2 === 0 ? "bg-slate-50" : undefined}>
+                      {items.map((item, index) => {
+                        const classes =
+                          typeof item === "number" ? "text-right" : "text-left";
+                        return (
+                          <TableCell key={index} className={classes}>
+                            {formatValue(item)}
+                          </TableCell>
+                        );
+                      })}
+                    </TableRow>
+                  ))
+                }
+              </TableBody>
+            </Table>
+          </TableRoot>
+        }
       </Card>
     </div>
   );
