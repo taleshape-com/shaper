c0236c0bd967bd34dcd4ef1323ae11891df71e2d
Jorin Vogel <hi@jorin.me>
2024-12-03 15:57:07 +0700

    fix lint

--- vite.embed.ts
+++ vite.embed.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
 import { visualizer } from "rollup-plugin-visualizer";
 import path from 'path';
 import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
-import prefixer from 'postcss-prefix-selector';
 
 export default defineConfig({
   plugins: [
