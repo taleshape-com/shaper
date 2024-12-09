6e600a985c85a89319e05503d4c971297f995fdc
Jorin Vogel <hi@jorin.me>
2024-12-09 14:55:30 +0700

    fix lint

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,18 +1,18 @@
 import { createRootRouteWithContext, Link, Outlet } from "@tanstack/react-router";
-import React from "react";
+//import React from "react";
 import { IAuthContext } from "../lib/auth";
 
-const TanStackRouterDevtools =
-  process.env.NODE_ENV === "production"
-    ? () => null // Render nothing in production
-    : React.lazy(() =>
-      // Lazy load in development
-      import("@tanstack/router-devtools").then((res) => ({
-        default: res.TanStackRouterDevtools,
-        // For Embedded Mode
-        // default: res.TanStackRouterDevtoolsPanel
-      })),
-    );
+//const TanStackRouterDevtools =
+//process.env.NODE_ENV === "production"
+//? () => null // Render nothing in production
+//: React.lazy(() =>
+//// Lazy load in development
+//import("@tanstack/router-devtools").then((res) => ({
+//default: res.TanStackRouterDevtools,
+//// For Embedded Mode
+//// default: res.TanStackRouterDevtoolsPanel
+//})),
+//);
 
 interface RouterContext {
   auth: IAuthContext
