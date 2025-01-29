21c3fead523f0b56345e32bd20b2e3761e218a04
Jorin Vogel <hi@jorin.me>
2025-01-29 18:37:10 +0700

    datepicker no shadow

--- src/components/tremor/DatePicker.tsx
+++ src/components/tremor/DatePicker.tsx
@@ -154,7 +154,7 @@ TimeInput.displayName = "TimeInput"
 const triggerStyles = tv({
   base: [
     // base
-    "peer flex w-full cursor-pointer appearance-none items-center gap-x-2 truncate rounded-md border px-2 py-1.5 shadow-sm outline-none transition-all text-sm",
+    "peer flex w-full cursor-pointer appearance-none items-center gap-x-2 truncate rounded-md border px-2 py-1.5 outline-none transition-all text-sm",
     // background color
     "bg-cbgl dark:bg-dbgl",
     // border color
