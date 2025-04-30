0f954d7393a2c4f53eedd0d222444fcbb3a71d10
Jorin Vogel <hi@jorin.me>
2025-04-30 12:38:01 +0700

    single query + single container renders full container height correctly

--- src/components/dashboard/index.tsx
+++ src/components/dashboard/index.tsx
@@ -242,6 +242,7 @@ const DataView = ({
         <section
           key={sectionIndex}
           className={cx("grid grid-cols-1 ml-4", {
+            "h-full": numContentSections === 1 && numQueriesInSection === 1,
             "@sm:grid-cols-2": numQueriesInSection > 1,
             "@lg:grid-cols-2":
               numQueriesInSection === 2 ||
@@ -267,8 +268,9 @@ const DataView = ({
                 className={cx(
                   "mr-4 mb-4 min-h-[320px] h-[calc(50dvh-2.90rem)] bg-cbgs dark:bg-dbgs border-none shadow-sm",
                   {
+                    "min-h-[calc(100%-1.2rem)] h-[calc(100%-1.2rem)]": numContentSections === 1 && numQueriesInSection === 1,
                     "@sm:h-[calc(100dvh-1.2rem)] @lg:min-h-[calc(50dvh-1.2rem)]":
-                      numQueriesInSection === 1,
+                      numContentSections > 1 && numQueriesInSection === 1,
                     "h-[calc(100dvh-1.2rem)] @sm:h-[calc(50dvh-1.2rem)] @lg:min-h-[calc(50dvh-1.2rem)] @lg:h-[calc(100%-1.2rem)]":
                       numContentSections === 1 && numQueriesInSection === 2,
                   },
