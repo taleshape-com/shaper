f5f9d4950786cfb4f904d7bd9026b022ab0d126f
Jorin Vogel <hi@jorin.me>
2026-05-14 18:41:48 +0700

    add jwt authz check

--- server/web/handler/validate.go
+++ server/web/handler/validate.go
@@ -7,6 +7,7 @@ import (
 	"shaper/server/core"
 	"time"
 
+	"github.com/golang-jwt/jwt/v5"
 	"github.com/labstack/echo/v4"
 )
 
@@ -23,6 +24,16 @@ type ValidateResponse struct {
 
 func Validate(app *core.App) echo.HandlerFunc {
 	return func(c echo.Context) error {
+		ctxUser := c.Get("user")
+		if ctxUser != nil {
+			claims := ctxUser.(*jwt.Token).Claims.(jwt.MapClaims)
+			if _, hasId := claims["dashboardId"]; hasId {
+				return c.JSONPretty(http.StatusUnauthorized, struct {
+					Error string `json:"error"`
+				}{Error: "Unauthorized"}, "  ")
+			}
+		}
+
 		var req ValidateRequest
 		if err := c.Bind(&req); err != nil {
 			return c.JSON(http.StatusBadRequest, map[string]string{"error": "Invalid request body"})
