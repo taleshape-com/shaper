ce5bb5bb8341eddcb300839f2143e6c1bf10b554
Jorin Vogel <hi@jorin.me>
2026-05-15 15:57:54 +0700

    handler token check

--- server/web/handler/schema.go
+++ server/web/handler/schema.go
@@ -6,11 +6,21 @@ import (
 	"net/http"
 	"shaper/server/core"
 
+	"github.com/golang-jwt/jwt/v5"
 	"github.com/labstack/echo/v4"
 )
 
 func GetSchema(app *core.App) echo.HandlerFunc {
 	return func(c echo.Context) error {
+		ctxUser := c.Get("user")
+		if ctxUser != nil {
+			claims := ctxUser.(*jwt.Token).Claims.(jwt.MapClaims)
+			if _, hasId := claims["dashboardId"]; hasId {
+				return c.JSONPretty(http.StatusUnauthorized, struct {
+					Error string `json:"error"`
+				}{Error: "Unauthorized"}, "  ")
+			}
+		}
 		res, err := app.GetSchema(c.Request().Context())
 		if err != nil {
 			return c.JSONPretty(http.StatusInternalServerError, struct {
